e9fe6166a5592e5a23195604c47f39c79dbfa745
stephie-spec <compwork254@gmail.com>
2025-09-21 20:29:28 +0300

    writes variables

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     buyTicketBtn.addEventListener('click', handleBuyTicket);
 });
+
+let currentMovie = null;
+let allMovies = [];
+
+const filmsList = document.getElementById('films');
+const poster = document.getElementById('poster');
+const title = document.getElementById('title');
+const runtime = document.getElementById('runtime');
+const description = document.getElementById('description');
+const showtime = document.getElementById('showtime');
+const ticketNum = document.getElementById('ticket-num');
+const buyTicketBtn = document.getElementById('buy-ticket');
+const movieDetailsLoading = document.getElementById('movie-details-loading');
